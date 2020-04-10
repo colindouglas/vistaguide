e287a9321be67df1414262095b30762a0568070a
colin <colindouglas@gmail.com>
2020-04-10 09:23:29 -0300

    Removed old scripts that weren't being used. Focus on get-withinday-listings.py

---
+++ get-withinday-listings.py
@@ -0,0 +1,33 @@
+from configparser import ConfigParser
+import viewpoint as vp
+
+# Read credentials from config file (./config.txt)
+config = ConfigParser('config.txt')
+config.read('config.txt')
+
+# Find the next available filename
+path = vp.next_filename("data/listings_")
+
+# Log into ViewPoint
+session = vp.Viewpoint(
+    username=config['credentials']['username'],
+    password=config['credentials']['password']
+)
+
+# Open the Dashboard for a text list
+session.find_element_by_link_text('DASHBOARD').click()
+vp.wait('Opening dashboard', 5)
+
+# Click on the 'Saved Searches' link
+session.find_element_by_link_text('SAVED SEARCHES').click()
+vp.wait('Opening Saved Searches index', 3)
+
+# Open the previously saved 'Halifax in the Last Week' search
+session.find_element_by_partial_link_text('Everything WithinDay').click()
+vp.wait('Opening relevant saved search', 3)
+
+# Scrape all of the data from the opened index
+session.scrape_all(out=path)
+
+# Close everything at the end
+session.quit()
--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 
-
 today=$(date +''%Y%m%d'')
 
 cd /home/colin/projects/viewpointer/
-venv/bin/python3 -u get-withinday-anystatus.py 2>&1 | tee -a logs/"${today}"_withinday.log
-# venv/bin/python3 get-new-today.py > logs/"${today}"_new-today.log
+
+venv/bin/python3 -u get-withinday-listings.py 2>&1 | tee -a logs/"${today}"_withinday.log
 
 Rscript --no-save --no-restore --verbose listing-cleanup.R 2>&1 | tee -a  logs/"${today}"_cleanup.log
