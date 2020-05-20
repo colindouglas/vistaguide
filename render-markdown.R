389b3432224addc75f402d425615ad98085cb046
Colin Douglas <colindouglas@gmail.com>
2020-05-19 21:08:49 -0300

    Update structure for external post

--- render-markdown.R
+++ render-markdown.R
@@ -1,29 +1,3 @@
-library(knitr)
-library(rmarkdown)
-
-# What format should the public-facing analysis be in?
-format <- "html"
-
-if (format == "md") {
-  rmarkdown::render("analysis.Rmd", output_file = "analysis.md", output_format = "md_document")
-  
-  body <- readLines("analysis.md")
-  body <- c(
-    "---",
-    "title: ViewPointer",
-    "permalink: /viewpointer/",
-    "layout: page",
-    "excerpt: Aggregated data on Nova Scotia real estate listings",
-    "comments: false",
-    "---", "", body)
-  
-  write(body, file = "analysis.md")
-  
-  system("./update-public.sh md")
-  
-}  else if (format == "html") {
-  
-  # Update the analysis markdown to HTML
-  rmarkdown::render("analysis.Rmd", output_file = "analysis.html", output_format = "html_document")
-  system("./update-public.sh html")
-}
+# Re-render all of the outputs with the current dataset
+# all == c("html", "markdown")
+rmarkdown::render("analysis.Rmd", output_format = "all")
