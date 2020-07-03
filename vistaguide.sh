a7ce59a1da6db66867bac6161d6a781c26cea21e
Colin Douglas <colindouglas@gmail.com>
2020-07-03 11:14:11 -0300

    whoops

--- vistaguide.sh
+++ vistaguide.sh
@@ -18,7 +18,7 @@ echo Logging to ${logfile}
 cd ${wd} || exit
 
 # Scrape today's data from ViewPoint
-#venv/bin/python3 -u 01-scrape-new-today.py
+venv/bin/python3 -u 01-scrape-new-today.py
 
 # Retry yesterday's failures
 venv/bin/python3 -u 02-retry-failures.py 
