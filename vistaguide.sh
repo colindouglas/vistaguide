6a024bb843f49c18357ae764a61b79fbd973b51a
Colin Douglas <colindouglas@gmail.com>
2020-07-06 15:05:52 -0300

    update analysis to include time-on-market

--- vistaguide.sh
+++ vistaguide.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
 
-# Set appropriate paths
-source /home/colin/.bashrc
-
 today=$(date +''%Y%m%d'')
 wd=/home/colin/projects/vistaguide  # Set the working directory
 logfile=${wd}/logs/console/"${today}".log
@@ -33,5 +30,5 @@ Rscript --no-save --no-restore --verbose 04-tidy-and-combine.R
 Rscript --no-save --no-restore --verbose 05-render-markdown.R
 
 # Deploy to web
-./06-deploy.sh 
+source 06-deploy.sh 
 
